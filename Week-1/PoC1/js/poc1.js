acf9af3a610d76458f696a911f7f9a134302f5f1
JoeThomas42 <132302844+JoeThomas42@users.noreply.github.com>
2024-08-23 09:54:53 -0400

    poc1 third attempt

--- Week-1/PoC1/js/poc1.js
+++ Week-1/PoC1/js/poc1.js
@@ -9,6 +9,9 @@ console.log(student);
 let quote = `PoC #1`;
 console.log(quote);
 
+quote = "She said, \"JavaScript is a 'cool' language.\"";
+console.log(quote);
+
 quote = 'She said, "JavaScript is a \'cool\' language."';
 console.log(quote);
 
@@ -19,13 +22,15 @@ quote = `${student} said, "JavaScript is a 'cool' language."`;
 console.log(quote);
 
 // Task 4
-let haiku = 'Chaos reigns within.\n' +
+let haiku =
+'Chaos reigns within.\n' +
 'Reflect, repent, and reboot.\n' +
 'Order shall return.';
 console.log(haiku);
 
 // Task 5
-let lyric = `There's a lady who's sure
+let lyric =
+`There's a lady who's sure
 all that glitters is gold
 and she's buying the stairway to heaven.`;
 console.log(lyric);
@@ -36,27 +41,34 @@ let totalBalls = 20;
 let ballsPerDog = Math.floor(totalBalls / totalDogs);
 let leftoverBalls = totalBalls % totalDogs;
 
-let dogSummary = `There are ${totalDogs} dogs and ${totalBalls} tennis balls.
+let dogSummary = 
+`There are ${totalDogs} dogs and ${totalBalls} tennis balls.
 Each dog will get ${ballsPerDog} tennis balls.
 There will be ${leftoverBalls} left over.`;
 console.log(dogSummary);
 
 // Task 7
-let tips = {
-  serverA: 22124,
-  serverB: 8241,
-  serverC: 22500,
-}
+let serverA = 221.24;
+let serverB = 82.41;
+let serverC = 225.00;
+
+serverA *= 100;
+serverB *= 100;
+serverC *= 100;
+
 let servers = 3;
 
-let tipsSum = tips.serverA + tips.serverB + tips.serverC;
+let tipsSum = serverA + serverB + serverC;
 let takeHome = Math.floor(tipsSum / servers);
 let leftoverTips = tipsSum % servers;
 
+tipsSum /= 100;
+takeHome /= 100;
+leftoverTips /= 100;
+
 let tipSummary = 
 `There are a total of ${servers} servers.
-The total tips sum to $${tipsSum / 100}.
-Each server will take home $${takeHome / 100}.
-$${leftoverTips / 100} will be contributed to the community fund.
-`;
+The total tips sum to $${tipsSum}.
+Each server will take home $${takeHome}.
+$${leftoverTips} will be contributed to the community fund.`;
 console.log(tipSummary);
