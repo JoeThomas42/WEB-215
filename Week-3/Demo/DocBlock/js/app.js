4b7d773f9015a9b377467db3eaa2c618eab727b8
JoeThomas42 <132302844+JoeThomas42@users.noreply.github.com>
2024-09-06 13:39:55 -0400

    DocBLock demo

--- Week-3/Demo/DocBlock/js/app.js
+++ Week-3/Demo/DocBlock/js/app.js
@@ -2,3 +2,28 @@
 
 // DocBlock is a standardized format of how to document your functions
 
+/**
+ * Hide an email from spambots
+ * 
+ * @param {string} user - left part of email address
+ * @param {string} domain - right part of email address
+ * @param {boolean} isAdmin - is this person a site administrator?
+ * 
+ * @returns {string} complete anchor tag
+ */
+
+function findAge(user, domain, isAdmin) {
+  let address = `${user}@${domain}`;
+  const EMAILLINK = document.createElement(`a`);
+  EMAILLINK.setAttribute(`href`, `mailto:${address}`);
+  EMAILLINK.innerHTML = `Send email to ${address}`;
+  if(isAdmin){
+    let adminMsg = ` (admin) `;
+    EMAILLINK.innerHTML += adminMsg;
+  }
+
+  console.log(EMAILLINK);
+  return EMAILLINK;
+}
+
+findAge(`Joe`, `gmail.com`, true);
